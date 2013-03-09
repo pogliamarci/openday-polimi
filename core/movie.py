ea67d77844267c4bee5c791ebb9c1e43971c5c14
Eros <eros@eros-laptop.(none)>
2013-03-09 19:03:52 +0100

    fixed param names in homepage.html and movie.py

--- core/movie.py
+++ core/movie.py
@@ -9,7 +9,7 @@ def render_page_content(movie_id):
 	movie = db.get_movie_by_id( movie_id )
 
 	content['title'] = movie['title']
-	content['actors'] = movie['title']
+	content['actors'] = movie['actors']
 	content['reviews'] = db.get_reviews_by_id( movie_id )
 
 	return render_template('movie.html', content = content)
